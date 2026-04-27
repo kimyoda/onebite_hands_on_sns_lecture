6cd6304149d15fe9e08319ed20606823ebe1ca6a
김요한 <rladygks1210@naver.com>
2026-04-28 01:55:37 +0900

    [feat] section07_캐시_조작_추가

--- section07/ONEBITE-LOG/src/hooks/mutations/post/use-create-post.ts
+++ section07/ONEBITE-LOG/src/hooks/mutations/post/use-create-post.ts
@@ -1,14 +1,24 @@
 import { createPost, createPostWithImages } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreatePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createPostWithImages,
     onSuccess: () => {
       if (callbacks?.onSuccess) {
         callbacks.onSuccess();
       }
+      // 1. 캐시를 아예 초기화(브라우저에서 추가하는 동작은 버튼을 누를 떄 추가되기에, 피드 자체를 지운 다음에 새롭게 불러오는게 적절하다고 판단)
+      // resetQueries를 통해 초기화 하고 다시 받아온다
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
+      // 2. 캐시 데이터에 오나성된 포스트만 추가
+      // 3. 낙관적 업데이트 방식(onMutate)
     },
     onError: (error) => {
       if (callbacks?.onError) {
