eade1a44cb78a66d741d2653ef19df91fb9d0b4a
김요한 <rladygks1210@naver.com>
2026-05-05 15:56:31 +0900

    [feat] section08_4_좋아요_결과_반영하기(낙관적업데이트)_추가

--- section08/ONEBITE-LOG/src/hooks/mutations/post/use-toggle-post-like.ts
+++ section08/ONEBITE-LOG/src/hooks/mutations/post/use-toggle-post-like.ts
@@ -1,20 +1,56 @@
 import { togglePostLike } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Post, UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useTogglePostLike(callbacks?: UseMutationCallback) {
   /*
     useMutation으로 togglePostLike를 감싼다.
     좋아요는 데이터를 변경하는 작업이므로 useMutation을 사용한다.
   */
+
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: togglePostLike,
+    // 낙관적 업데이트 추가
+    onMutate: async ({ postId }) => {
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.post.byId(postId),
+      });
+
+      const prevPost = queryClient.getQueryData<Post>(
+        QUERY_KEYS.post.byId(postId),
+      );
+
+      // 캐시데이터 낙관적 업데이트
+      queryClient.setQueryData<Post>(QUERY_KEYS.post.byId(postId), (post) => {
+        if (!post) {
+          throw new Error("포스트가 존재하지 않습니다");
+        }
+        return {
+          ...post,
+          isLiked: !post.isLiked,
+          like_count: post.isLiked ? post.like_count - 1 : post.like_count + 1,
+        };
+      });
+
+      return {
+        prevPost,
+      };
+    },
     onSuccess: () => {
       if (callbacks?.onSuccess) {
         callbacks.onSuccess();
       }
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context && context.prevPost) {
+        queryClient.setQueryData(
+          QUERY_KEYS.post.byId(context.prevPost.id),
+          context.prevPost,
+        );
+      }
       if (callbacks?.onError) {
         callbacks.onError(error);
       }
